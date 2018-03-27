f8f5fb4f3ccd7e41238cee5dd3c8c648940e8210
Saverio Tosi <s.tosi@extrategy.net>
2018-03-27 17:40:56 +0200

    refactoring folder tree

--- src/Infrastructure/Controller/FoodController.php
+++ src/Infrastructure/Controller/FoodController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controller;
+namespace App\Infrastructure\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
